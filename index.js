37a8e63c68a10fb6cc308ffb52de8e176d3de366
headlulu888 <headlulu888@gmail.com>
2019-05-09 22:12:04 +0300

    create web page

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.end('Hello form nodejs')
+    res.render('index')
 })
 
 app.listen(3000, () => {
